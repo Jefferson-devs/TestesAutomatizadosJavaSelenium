03b3c74ddcbf37a0b658bad007cd614f68be838f
JeffersonSoares <Jeffersonsoares.s@yahoo.com>
2022-05-20 09:52:29 -0300

    Encapsulamento em POO

---
+++ src/test/java/automatizado/test/BaseTest.java
@@ -0,0 +1,28 @@
+package automatizado.test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public abstract class BaseTest { //Encapsulamento em POO
+
+    protected static WebDriver driver;  //Esse private vai saber iniciar o nosso navegador
+    private static final String URL_BASE = "https://www.google.com.br/" ;  //URL_base, site que eu quero que abra com a automação
+    private static final String CAMINHO_DRIVER = "src/test/java/automatizado/resource/chromedriver-v1010495141.exe";  //Adicionar o caminho de onde está o driver 
+
+    @BeforeClass // Essa classe (metodo) vai ser chamada antes de qualquer outra classe (Antes)
+    public static void iniciar(){  //Inicia o driver e navega até a url antes mesmo de fazer o CT.
+        System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);  
+         driver = new ChromeDriver(); // Instanciando o driver par abrir o navegador
+         driver.manage().window().maximize();
+         driver.get(URL_BASE); // Quero que esta URL navegue até a url base
+    } 
+
+    @AfterClass //Este metodo só roda no final depois que todos os CT.s (as classes) terminar (Depois)
+    public static void finalizar() {
+        driver.quit();
+    }
+}
+
+
--- src/test/java/automatizado/test/GoogleTest.java
+++ src/test/java/automatizado/test/GoogleTest.java
@@ -5,27 +5,13 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class GoogleTest {
-
-    private WebDriver driver;  //Esse private vai saber iniciar o nosso navegador
-    private final String URL_BASE = "https://www.google.com.br/" ;  //URL_base, site que eu quero que abra com a automação
-    private final String CAMINHO_DRIVER = "src/test/java/automatizado/resource/chromedriver-v1010495141.exe";  //Adicionar o caminho de onde está o driver
 
 
-    private void iniciar(){  //Inicia o driver e navega até a url antes mesmo de fazer o CT.
-        System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);  
-         driver = new ChromeDriver();  //Instanciando o driver par abrir o navegador
-         driver.manage().window().maximize();
-         driver.get(URL_BASE); // Quero que esta URL navegue até a url base
-    } 
+public class GoogleTest extends BaseTest { //Tudo que estiver em BaseTest será acessivel pelo GoogleTest
 
     @Test //Indica que é um caso de teste
     public void devePesquisarNoGoogle(){ //Criando um metodo 
-        iniciar();
 
         WebElement inputPesquisa = driver.findElement(By.name("q")); //Elemento da web
         inputPesquisa.sendKeys("Batata frita" + Keys.ENTER); //Irá escrever no navegador
@@ -34,6 +20,17 @@ public class GoogleTest {
 
         assertTrue(resultado, resultado.contains("Aproximadamente")); //Testa para mim se é verdade se dentro de resultado..
 
-        driver.quit();
+    }
+
+    @Test //Indica que é um caso de teste
+    public void devePesquisarNoGoogle2(){ //Criando um metodo 
+
+        WebElement inputPesquisa = driver.findElement(By.name("q")); //Elemento da web
+        inputPesquisa.sendKeys("Batata frita" + Keys.ENTER); //Irá escrever no navegador
+
+        String  resultado = driver.findElement(By.id("result-stats")).getText(); //Retorna o texto de um elemento getText
+
+        assertTrue(resultado, resultado.contains("Aproximadamente")); //Testa para mim se é verdade se dentro de resultado..
+
     }
 }   
